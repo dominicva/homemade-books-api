95a76c924ad49f02986a4ccec97d8214ef4e720d
Dom van Almsick <dominicvana@gmail.com>
2022-06-09 17:03:54 -0500

    feat: get book by id

--- src/server.js
+++ src/server.js
@@ -6,10 +6,19 @@ const app = express();
 app.use(json());
 app.use(urlencoded({ extended: true }));
 
-app.get('/', async (req, res) => {
+app.get('/books', async (req, res) => {
   const sql = 'select * from books';
   const result = await db.query(sql);
   res.json({ data: result.rows });
+});
+
+app.get('/books/:id', async (req, res) => {
+  const { id } = req.params;
+  const sql = 'SELECT * FROM books WHERE book_id = $1';
+
+  const { rows } = await db.query(sql, [id]);
+
+  res.send({ data: rows });
 });
 
 export default app;
